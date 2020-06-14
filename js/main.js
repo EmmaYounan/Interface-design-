6dbb3bac3ee14481a476d58d5a2f803638fa0e82
emma younan <emma-y@hotmail.com>
2020-06-15 01:05:12 +0200

    animation test

--- js/main.js
+++ js/main.js
@@ -11,16 +11,17 @@ var secondImage = document.querySelector(".second-img");
 var div = document.querySelector(".second-section div:nth-of-type(2)")
 var div2 = document.querySelector(".second-section div:nth-of-type(1)")
 var backButton = document.querySelector(".second-section button")
+var img = document.querySelector('.second-img')
 
 secondImage.addEventListener("click", function(){
-    secondImage.src= "images/full-width.png"   
-    div.classList.add('full-width')
+    img.classList.add("full-image")
+    div.classList.add('animate')
     div2.classList.add('appear')
     backButton.classList.add("show")
 })
 backButton.addEventListener('click', function(){
-    secondImage.src= "images/first.png"
-    div.classList.remove('full-width')
+    img.classList.remove("full-image")
+    div.classList.remove('animate')
     div2.classList.remove('appear')
     backButton.classList.remove("show")
 })

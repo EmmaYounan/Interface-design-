7d6e62073d55179cd436820ce8cdaa371ee40c11
emma younan <emma-y@hotmail.com>
2020-06-16 19:41:21 +0200

    styling in progress

--- js/main.js
+++ js/main.js
@@ -11,14 +11,23 @@ var secondImage = document.querySelector(".second-img");
 var div = document.querySelector(".second-section div:nth-of-type(2)")
 var div2 = document.querySelector(".second-section div:nth-of-type(1)")
 var backButton = document.querySelector(".second-section button")
+var glases = document.querySelector("#Layer_2")
+var logo = document.querySelector(".logo")
+var footer = document.querySelector("footer")
 
 secondImage.addEventListener("click", function(){
+    logo.classList.add("hide")
+    glases.classList.add("hide")
+    footer.classList.add("hide")
     secondImage.classList.add("full-image")
     div.classList.add('animate')
     div2.classList.add('appear')
     backButton.classList.add("show")
 })
 backButton.addEventListener('click', function(){
+    logo.classList.remove("hide")
+    glases.classList.remove("hide")
+    footer.classList.remove("hide")
     secondImage.classList.remove("full-image")
     div.classList.remove('animate')
     div2.classList.remove('appear')
@@ -33,6 +42,9 @@ var div4 = document.querySelector(".dream div:nth-of-type(1)")
 var thirdH2 = document.querySelector(".third-h2")
 
 thirdImage.addEventListener("click", function(){
+    logo.classList.add("hide")
+    glases.classList.add("hide")
+    footer.classList.add("hide")
     thirdImage.classList.add("full-image")
     balerina.src="images/balerina.gif"
     balerina.classList.add("balerina")
@@ -42,6 +54,9 @@ thirdImage.addEventListener("click", function(){
     backButton2.classList.add("show")
 })
 backButton2.addEventListener('click', function(){
+    logo.classList.remove("hide")
+    glases.classList.remove("hide")
+    footer.classList.remove("hide")
     thirdImage.classList.remove("full-image")
     balerina.src="images/ballet.png"
     balerina.classList.remove("balerina")
@@ -53,6 +68,32 @@ backButton2.addEventListener('click', function(){
 
 console.log(window.innerWidth)
 
+
+
+var cotonImage = document.querySelector(".coton-girl")
+var backButtonCoton = document.querySelector(".coton button")
+var div5 = document.querySelector(".coton div:nth-of-type(2)")
+var div6 = document.querySelector(".coton div:nth-of-type(1)")
+var sectionCoton = document.querySelector(".coton")
+
+cotonImage.addEventListener("click", function(){
+    logo.classList.add("hide")
+    glases.classList.add("hide")
+    footer.classList.add("hide")
+    sectionCoton.classList.add('background-coton')
+    div5.classList.add('animate')
+    div6.classList.add('appear')
+    backButtonCoton.classList.add("show")
+})
+backButtonCoton.addEventListener('click', function(){
+    logo.classList.remove("hide")
+    glases.classList.remove("hide")
+    footer.classList.remove("hide")
+    sectionCoton.classList.remove('background-coton')
+    div5.classList.remove('animate')
+    div6.classList.remove('appear')
+    backButtonCoton.classList.remove("show")
+})
 
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml6 .letters');
